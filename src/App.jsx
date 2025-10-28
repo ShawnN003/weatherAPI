72ef3e3bf15a793111e588b0fc846375677eca26
BradyLeg <184549450+BradyLeg@users.noreply.github.com>
2025-10-28 13:43:45 -0700

    api fectch

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,24 @@ import { useEffect } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [countryCode, setCode] = useState("co");
+  const [countryData, setData] = useState();
+
+  useEffect(function () {
+    const fetchData = async () => {
+      try {
+        const res = await fetch(`https://restcountries.com/v3.1/${countryCode}`);
+        const data = await res.json();
+        setData(data);
+
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+
+    fetchData();
+  }, [])
 
   return (
     
